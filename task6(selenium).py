79525c81ed351900e3ef446bb82480c284d4575a
Angelina <angelinyshechka@mail.ru>
2023-04-09 00:21:53 +0300

    new

--- task6(selenium).py
+++ task6(selenium).py
@@ -11,13 +11,15 @@ browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 # time.sleep(5)
 # 7.1. Найдите кпопку c текстом "Gold". Попробуйте подобрать как минимум 2 разных XPATH и/или CSS-селекторов
 # http://suninjuly.github.io/xpath_examples
-# browser.get('http://suninjuly.github.io/xpath_examples')
-#
-# a = browser.find_element(By.XPATH,"/html/body/div[2]/button[3]")
-# print(a.text)
-# a1 = browser.find_element(By.XPATH,"//*[contains(text(), 'Gold')]")
-# print(a1.text)
-#
+browser.get('http://suninjuly.github.io/xpath_examples')
+
+a = browser.find_element(By.XPATH,"/html/body/div[2]/button[3]")
+print(a.text)
+a1 = browser.find_element(By.XPATH,"//*[contains(text(), 'Gold')]")
+print(a1.text)
+#(//button)[7]
+#(//button)[last()-1]
+#//*[text()='Gold']
 # # 7.2. Найдите элемент с текстом "Fully charged cat". Чем больше разных XPATH и/или CSS-селекторов сможете подобрать, тем лучше
 # # http://suninjuly.github.io/cats.html
 browser.get('http://suninjuly.github.io/cats.html')
@@ -26,3 +28,7 @@ a3 = browser.find_element(By.XPATH,"//*[contains(text(), 'Fully charged cat')]")
 print(a3.text)
 a4 = browser.find_element(By.XPATH,"/html/body/main/div/div/div/div[5]/div/div/p")
 print(a4.text)
+#//*[text()='Fully charged cat']
+#(//p)[5]
+#(//p[@class])[5]
+#//*[.='Fully charged cat']
