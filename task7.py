1313115750ca221376631967ce65c2448d10f643
Angelina <angelinyshechka@mail.ru>
2023-04-24 22:58:08 +0300

    add

---
+++ task7.py
@@ -0,0 +1,26 @@
+Напишите любой user-story и соответствующий тест кейс для сайта https://openweathermap.org/
+
+*** user-story:
+
+As a User, I want to visit the Main page, so that I can see the website’s name and description at the top of the page.
+
+Acceptance criteria:
+
+Verify the website’s name is "OpenWeather" and it's visible;
+Verify the website’s description is 'Weather forecasts, nowcasts and history in a fast and elegant way' and it's visible.
+
+
+*** test case:
+
+Precondition:
+Navigate to Main page https://openweathermap.org/‌
+
+Steps:
+
+Find website's name and description
+Verify the website's name and description
+
+Expected results:
+
+The website's name visible and matches expected text (OpenWeather)
+The website's description visible and matches expected text (Weather forecasts, nowcasts and history in a fast and elegant way.)
